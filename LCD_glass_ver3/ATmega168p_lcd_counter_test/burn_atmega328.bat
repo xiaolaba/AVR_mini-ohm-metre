66fab6ae17859ea96ba597f4b76b4a3ec581315b
xiaolaba <xiao_laba_cn@yahoo.com>
2020-08-27 08:46:38 +0800

    Add files via upload

---
+++ LCD_glass_ver3/ATmega168p_lcd_counter_test/burn_atmega328.bat
@@ -0,0 +1,16 @@
+@echo off
+set path=
+
+
+set mcu=atmega328p
+
+set main=main
+set lcd=lcd
+set out=AVR_lcd_%mcu%
+set ac=C:\WinAVR-20100110
+
+path %ac%\bin;%ac%\utils\bin;%path%;
+
+avrdude -c usbtiny -p %mcu% -U flash:w:%out%.hex":a
+pause
+:end
